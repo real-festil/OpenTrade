a638500fc647113e22aa456dcc1170c139395810
Gladyshev Roman <53917776+real-festil@users.noreply.github.com>
2020-01-07 17:10:49 +0600

    Add files via upload

---
+++ build/precache-manifest.830700bc0b8053af534ea36f24d30483.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "814ea5b5d5891227506b10c2302eb3e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c880c21385de748dffd5",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a67270036420159d5da0",
+    "url": "/static/js/2.2b943474.chunk.js"
+  },
+  {
+    "revision": "e1c54c01bade16d0a6a047e212e2b3a7",
+    "url": "/static/js/2.2b943474.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c880c21385de748dffd5",
+    "url": "/static/js/main.17f7204e.chunk.js"
+  },
+  {
+    "revision": "f37662605f41577669b9",
+    "url": "/static/js/runtime-main.0d300568.js"
+  }
+]);
